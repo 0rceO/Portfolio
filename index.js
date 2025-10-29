66c84081569d0a2a6308ced3c18f411d334ea612
0rceO <137679577+0rceO@users.noreply.github.com>
2025-10-29 17:33:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("unavailable").addEventListener("click",function(){
+    alert("Currently Unavailable or Coming Soon")
+})
+
+document.getElementById("unavailable2").addEventListener("click",function(){
+    alert("Currently Unavailable or Coming Soon")
+})
